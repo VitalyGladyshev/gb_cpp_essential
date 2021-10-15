29c8c06e0fc167549b0607ed842f3aa44dfeb8fb
Vitaly Gladyshev <viv232@yandex.ru>
2021-10-15 22:34:46 +0300

    ДЗ 7 начал

---
+++ hw_07/hw_07.cpp
@@ -0,0 +1,33 @@
+//
+// Created by vitaly on 15.10.2021.
+//
+
+# include "mylib.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    //Задание 1/5
+    cout << "Задание 1/5" << endl;
+
+    cout << endl;
+
+    //Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+
+    //Задание 3
+    cout << "Задание 3" << endl;
+
+    cout << endl;
+
+    //Задание 4
+    cout << "Задание 4" << endl;
+
+    cout << endl;
+}
---
+++ hw_07/mylib.h
@@ -0,0 +1,8 @@
+//
+// Created by vitaly on 15.10.2021.
+//
+
+#ifndef GB_CPP_ESSENTIAL_MYLIB_H
+#define GB_CPP_ESSENTIAL_MYLIB_H
+
+#endif //GB_CPP_ESSENTIAL_MYLIB_H
